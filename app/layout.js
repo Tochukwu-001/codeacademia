6e2b2728febc9bc508a55d8111e700c4157c9776
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-03-30 15:22:43 +0100

    updates

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Merriweather } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const merriWeather = Merriweather({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: ["300", "400", "500", "600", "700", "800", "900"]
+})
 
 export const metadata = {
   title: "Codeacademia | Learn tech fast",
@@ -22,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${merriWeather.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
---
+++ app/signin/page.jsx
@@ -0,0 +1,29 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa6";
+import { FaApple } from "react-icons/fa";
+
+export default function Signin () {
+    return (
+        <main className="min-h-dvh flex items-center justify-center">
+            <section className="p-4 rounded-md shadow-md w-1/2 flex flex-col items-center gap-5">
+                <h1>Sign in to your Account</h1>
+                <form className="flex flex-col gap-3 w-1/2">
+                    <input type="text" placeholder="example@gmail.com" className="w-full outline-none border border-purple-600 rounded-md px-4 py-2"/>
+                    <button className="w-full bg-purple-600 text-white p-2 rounded-md hover:-translate-y-1 transition-all duration-200">Sign In</button>
+                </form>
+                
+                <div className="border w-full">
+                    <div></div>
+                    <p>or</p>
+                    <div></div>
+                </div>
+
+                <div>
+                    <button><FcGoogle /></button>
+                    <button><FaGithub /></button>
+                    <button><FaApple /></button>
+                </div>
+            </section>
+        </main>
+    )
+}

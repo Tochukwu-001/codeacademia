9d7dfed602a463bf77770be0102e9713c715e6a2
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-04-20 15:56:04 +0100

    updates

--- app/feed/[id]/page.jsx
+++ app/feed/[id]/page.jsx
@@ -2,23 +2,33 @@ import { db } from '@/config/firebase.config'
 import { doc, getDoc } from 'firebase/firestore'
 import React from 'react'
 
-const singlePost = async (id) => {
-    try {
-        const docRef = doc(db, "tutorials", id)
-        const docSnap = await getDoc(docRef)
+const singleReview = async (id) => {
+  if (!id) return null;
 
-        if (docSnap.exists()) {
-            return {id, ...doc.data()}
-        } else{
-            console.log("Document not found");
-        }
-    } catch (error) {
-        console.error("Error>>>>>>", error)
-        alert("Something went wrong")
+  try {
+    const docRef = doc(db, "tutorials", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      // console.log("Document data:", docSnap.data());
+      return { id, ...docSnap.data() };
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
     }
-}
+  } catch (error) {
+    console.error("An error occurred", error);
+    alert("Oops, somthing went wrong");
+  }
+};
 
-const page = () => {
+const page = async ({params}) => {
+    const sortedParams = await params
+    const singleDoc = await singleReview(sortedParams.id)
+    console.log(singleDoc);
+    // console.log(params);
+    
+    
   return (
     <div>
       Dynamic page
